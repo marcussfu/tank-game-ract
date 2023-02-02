d4a86387ab13c3ea9f9e9a3fda8a59afabb8df37
marcusfu <marcussfu@gmail.com>
2023-02-02 20:51:12 +0800

    adjust app relative css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import RotateWarning from './components/rotate-warning/rotate-warning.component';
 import World from './components/world/world.component';
-import { autocompleteClasses } from '@mui/material';
+
+import './App.styles.scss';
 
 function App() {
   return (
-    <div style={{
-      position: "relative",
-      maxWidth: '800px',
-      // paddingTop: '50%',
-      aspectRatio: '1.66/1',
-      margin: '5% auto'
-    }}>
+    <div className='app-container'>
         <RotateWarning />
         <World />
     </div>
