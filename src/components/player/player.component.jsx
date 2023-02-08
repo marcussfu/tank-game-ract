27ec03f7fc6f64bb5405056740193e1da9844761
marcusfu <marcussfu@gmail.com>
2023-02-08 22:35:04 +0800

    test joystick

--- src/components/player/player.component.jsx
+++ src/components/player/player.component.jsx
@@ -6,23 +6,23 @@ import {getCurrentPosition, obeserveBoundaries, directionToRotateDegree} from '.
 import store from '../../store/store';
 import playerTank from '../../assets/tank/playerTank.png';
 
-import shoot_by_player from '../../assets/sounds/shoot_by_player.mp3';
+// import shoot_by_player from '../../assets/sounds/shoot_by_player.mp3';
 import {SPRITE_SIZE, MAP_HEIGHT, MAP_WIDTH} from '../../config/constants';
 import './player.styles.scss';
 
 // const shootByPlayerAudio = new Audio(shoot_by_player);
 
-const Player = ({player}) => {
-    const {position, direction, hidden=false, spriteLocation, walkIndex} = player;
-    const tiles = useSelector(state => state.mapReducer.tiles);
-    const shootVolume = useSelector(state => state.settingReducer.shootVolume);
-    const {setTiles, setBullet, movePlayer,
-        removeTanks, gameWin} = useActions();
+const Player = ({player, fireHandler, attemptMove, moveHandler, stopHandler}) => {
+    const {position, direction, hidden=false, spriteLocation, walkIndex, rotate, posRatio} = player;
+    // const tiles = useSelector(state => state.mapReducer.tiles);
+    // const shootVolume = useSelector(state => state.settingReducer.shootVolume);
+    // const {setTiles, setBullet, movePlayer,
+    //     removeTanks, gameWin} = useActions();
 
     const [newDir, setNewDir] = useState('');
-    const [bulletShootedCount, setBulletShootedCount] = useState(0);
-    const [rotate, setRotate] = useState(0);
-    const [posRatio, setPosRatio] = useState({widthRatio: 1, heightRatio: 1});
+    // const [bulletShootedCount, setBulletShootedCount] = useState(0);
+    // const [rotate, setRotate] = useState(0);
+    // const [posRatio, setPosRatio] = useState({widthRatio: 1, heightRatio: 1});
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
@@ -34,82 +34,84 @@ const Player = ({player}) => {
     }, []);
 
     useEffect(() => {
-        setPosRatio({
-            widthRatio: document.getElementsByClassName('bullets-container')[0].offsetWidth/MAP_WIDTH,
-            heightRatio: document.getElementsByClassName('bullets-container')[0].offsetHeight/MAP_HEIGHT
-        });
-
+        console.log("BBBBBBBBB   ");
+    //     setPosRatio({
+    //         widthRatio: document.getElementsByClassName('bullets-container')[0].offsetWidth/MAP_WIDTH,
+    //         heightRatio: document.getElementsByClassName('bullets-container')[0].offsetHeight/MAP_HEIGHT
+    //     });
+        if (newDir === '') return;
         attemptMove(newDir);
+        setNewDir('');
     }, [newDir]);
 
-    useEffect(() => {
-        fireBullet(bulletShootedCount);
-    }, [bulletShootedCount]);
+    // useEffect(() => {
+    //     fireBullet(bulletShootedCount);
+    // }, [bulletShootedCount]);
 
-    const getSpriteLocation = (direction, walkIndex) => {
-        switch(direction) {
-            case 'SOUTH':
-                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*0}px`
-            case 'EAST':
-                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*1}px`
-            case 'WEST':
-                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*2}px`
-            case 'NORTH':
-                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*3}px`
-            default:
-                return "0px 0px"
-        }
-    }
+    // const getSpriteLocation = (direction, walkIndex) => {
+    //     switch(direction) {
+    //         case 'SOUTH':
+    //             return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*0}px`
+    //         case 'EAST':
+    //             return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*1}px`
+    //         case 'WEST':
+    //             return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*2}px`
+    //         case 'NORTH':
+    //             return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*3}px`
+    //         default:
+    //             return "0px 0px"
+    //     }
+    // }
 
-    const obeserveImpassable = (newPos, tiles) => {
-        const y = newPos[1] / SPRITE_SIZE;
-        const x = newPos[0] / SPRITE_SIZE;
-        const nextTile = tiles[y][x];
+    // const obeserveImpassable = (newPos, tiles) => {
+    //     const y = newPos[1] / SPRITE_SIZE;
+    //     const x = newPos[0] / SPRITE_SIZE;
+    //     const nextTile = tiles[y][x];
         
-        if (nextTile === 4) {
-            tiles[y][x] = 0;
-            setTiles(tiles);
-            gameWin();
-            removeTanks();
-        }
-        return nextTile < 5;
-    };
+    //     if (nextTile === 4) {
+    //         tiles[y][x] = 0;
+    //         setTiles(tiles);
+    //         gameWin();
+    //         removeTanks();
+    //     }
+    //     return nextTile < 5;
+    // };
 
-    function dispatchMove(dir, pos) {
-        const newWalkIndex = walkIndex >= 1? 0: walkIndex+1;
-        const newSpriteLocation = getSpriteLocation(dir, newWalkIndex);
+    // function dispatchMove(dir, pos) {
+    //     const newWalkIndex = walkIndex >= 1? 0: walkIndex+1;
+    //     const newSpriteLocation = getSpriteLocation(dir, newWalkIndex);
         
-        movePlayer({
-            position: pos,
-            direction: dir,
-            spriteLocation: newSpriteLocation,
-            walkIndex: newWalkIndex
-        });
-        console.log("player moves to " + pos)
-    }
+    //     movePlayer({
+    //         position: pos,
+    //         direction: dir,
+    //         spriteLocation: newSpriteLocation,
+    //         walkIndex: newWalkIndex
+    //     });
+    //     console.log("player moves to " + pos)
+    // }
 
-    const attemptMove = (dir) => {
-        if (dir === '') return;
-        setRotate(directionToRotateDegree(dir));
-        const newPos = getCurrentPosition(dir, position);
-        dispatchMove(dir, 
-            (obeserveBoundaries(newPos) && obeserveImpassable(newPos, tiles)? 
-                newPos: position));
-        setNewDir('');
-    }
+    // const attemptMove = (dir) => {
+    //     if (dir === '') return;
+    //     setRotate(directionToRotateDegree(dir));
+    //     const newPos = getCurrentPosition(dir, position);
+    //     dispatchMove(dir, 
+    //         (obeserveBoundaries(newPos) && obeserveImpassable(newPos, tiles)? 
+    //             newPos: position));
+    //     setNewDir('');
+    // }
 
-    const fireBullet = (currBulletCount) => {
-        if (currBulletCount <= 0) return;
-        setBullet({
-            position: position,
-            direction: direction,
-            key_index: 'tank_player_Bullet_' + currBulletCount,
-            is_player: true
-        });
-        const shootByPlayerAudio = new Audio(shoot_by_player);
-        shootByPlayerAudio.volume = shootVolume;
-        shootByPlayerAudio.play();
-    }
+    // const fireBullet = (currBulletCount) => {
+    //     if (currBulletCount <= 0) return;
+    //     setBullet({
+    //         position: position,
+    //         direction: direction,
+    //         key_index: 'tank_player_Bullet_' + currBulletCount,
+    //         is_player: true
+    //     });
+    //     const shootByPlayerAudio = new Audio(shoot_by_player);
+    //     shootByPlayerAudio.volume = shootVolume;
+    //     shootByPlayerAudio.play();
+    // }
     
     const handleKeyDown = (e) => {
         e.preventDefault();
@@ -120,13 +122,13 @@ const Player = ({player}) => {
                 // case 32:
                 //     return setBulletShootedCount(bulletShootedCount => bulletShootedCount+1);
                 case 37: case 65:
-                    return setNewDir('WEST');
+                    return moveHandler('WEST');
                 case 38: case 87:
-                    return setNewDir('NORTH');
+                    return moveHandler('NORTH');
                 case 39: case 68:
-                    return setNewDir('EAST');
+                    return moveHandler('EAST');
                 case 40: case 83:
-                    return setNewDir('SOUTH');
+                    return moveHandler('SOUTH');
                 default:
                     return console.log(e.keyCode);
             }
@@ -138,10 +140,13 @@ const Player = ({player}) => {
         // get current state
         if (!store.getState().worldReducer.game_over && 
             !store.getState().worldReducer.game_win) {
+            // stopHandler();
             switch (e.keyCode) {
                 case 32: case 13:
-                    return setBulletShootedCount(bulletShootedCount => bulletShootedCount+1);
+                    return fireHandler();
+                //     return setBulletShootedCount(bulletShootedCount => bulletShootedCount+1);
                 default:
+                    stopHandler();
                     return console.log(e.keyCode);
             }
         }
--- src/components/world/world.component.jsx
+++ src/components/world/world.component.jsx
@@ -18,11 +18,15 @@ import {setupTiles} from '../../config/functions';
 import {tiles} from '../../config/maps/map_1';
 
 import bgm from '../../assets/sounds/bgm.mp3';
+import shoot_by_player from '../../assets/sounds/shoot_by_player.mp3';
 import short_of_time_bgm from '../../assets/sounds/short_of_time_bgm.mp3';
 import game_over_bgm from '../../assets/sounds/game_over_bgm.mp3';
 import game_win_bgm from '../../assets/sounds/game_win_bgm.mp3';
 
 import enemyTank from '../../assets/tank/enemyTank.png';
+
+import {SPRITE_SIZE, MAP_HEIGHT, MAP_WIDTH} from '../../config/constants';
+import {getCurrentPosition, obeserveBoundaries, directionToRotateDegree} from '../../config/functions';
 
 import './world.styles.scss';
 
@@ -34,7 +38,7 @@ const World = () => {
     const {game_over, game_win, game_start, short_of_time, game_pause} = useSelector(state => state.worldReducer);
     const bgVolume = useSelector(state => state.settingReducer.bgVolume);
 
-    // const tiles = useSelector(state => state.mapReducer.tiles);
+    const currTiles = useSelector(state => state.mapReducer.tiles);
     const shootVolume = useSelector(state => state.settingReducer.shootVolume);
 
     // const bgmAudio = new Audio(bgm);
@@ -43,10 +47,41 @@ const World = () => {
 
     const [bgmAudio, setBgmAudio] = useState(new Audio(bgm));
 
+    const moveKeys = {
+        'LEFT': 'WEST',
+        'FORWARD': 'NORTH',
+        'RIGHT': 'EAST',
+        'BACKWARD': 'SOUTH'
+    }
+
     const [newDir, setNewDir] = useState('');
     const [bulletShootedCount, setBulletShootedCount] = useState(0);
     const [rotate, setRotate] = useState(0);
     const [posRatio, setPosRatio] = useState({widthRatio: 1, heightRatio: 1});
+
+    let player_move_interval = null;
+
+    useEffect(() => {
+        fireBullet(bulletShootedCount);
+    }, [bulletShootedCount]);
+
+    useEffect(() => {
+        setPosRatio({
+            widthRatio: document.getElementsByClassName('playground-container')[0].offsetWidth/MAP_WIDTH,
+            heightRatio: document.getElementsByClassName('playground-container')[0].offsetHeight/MAP_HEIGHT
+        });
+        
+        if (!player_move_interval) {
+            player_move_interval = setInterval(() => {
+                if (newDir == '') return;
+                attemptMove(newDir);
+            }, 200);
+        }
+        else if (player_move_interval && newDir == '') {
+            clearInterval(player_move_interval);
+        }
+        return () => clearInterval(player_move_interval);
+    }, [newDir, player]);
 
     useEffect(() => {
         bgmAudio.volume = bgVolume;
@@ -146,18 +181,88 @@ const World = () => {
         }
     };
 
-    const handleMove = (e) => {
-        console.log("move  ",e);
-        // handleClick(moveKeys[e.direction]);
+    const obeserveImpassable = (newPos, tiles) => {
+        const y = newPos[1] / SPRITE_SIZE;
+        const x = newPos[0] / SPRITE_SIZE;
+        const nextTile = tiles[y][x];
+        
+        if (nextTile === 4) {
+            tiles[y][x] = 0;
+            setTiles(tiles);
+            gameWin();
+            removeTanks();
+        }
+        return nextTile < 5;
     };
 
-    const handleStop = () => {
+    function dispatchMove(dir, pos) {
+        const newWalkIndex = player.walkIndex >= 1? 0: player.walkIndex+1;
+        const newSpriteLocation = getSpriteLocation(dir, newWalkIndex);
+        
+        movePlayer({
+            position: pos,
+            direction: dir,
+            spriteLocation: newSpriteLocation,
+            walkIndex: newWalkIndex
+        });
+        // console.log("player moves to " + pos)
+    }
+
+    const getSpriteLocation = (direction, walkIndex) => {
+        switch(direction) {
+            case 'SOUTH':
+                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*0}px`
+            case 'EAST':
+                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*1}px`
+            case 'WEST':
+                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*2}px`
+            case 'NORTH':
+                return `${SPRITE_SIZE*walkIndex}px ${SPRITE_SIZE*3}px`
+            default:
+                return "0px 0px"
+        }
+    }
+    
+    const moveHandler = (direction) => {
+        console.log("move  ", direction);
+        setNewDir(direction);
+    };
+
+    const stopHandler = () => {
         console.log("stop  ");
+        setNewDir('');
     };
 
     const fireHandler = () => {
         console.log("fire");
+        setBulletShootedCount(bulletShootedCount => bulletShootedCount+1);
     };
+
+    const attemptMove = (dir) => {
+        // if (dir === '') return;
+        setRotate(directionToRotateDegree(dir));
+        const newPos = getCurrentPosition(dir, player.position);
+        console.log("attemptMove    ", player.position, newPos);
+        
+        dispatchMove(dir, 
+            (obeserveBoundaries(newPos) && obeserveImpassable(newPos, currTiles)? 
+                newPos: player.position));
+        
+        // setNewDir('');
+    }
+
+    const fireBullet = (currBulletCount) => {
+        if (currBulletCount <= 0) return;
+        setBullet({
+            position: player.position,
+            direction: player.direction,
+            key_index: 'tank_player_Bullet_' + currBulletCount,
+            is_player: true
+        });
+        const shootByPlayerAudio = new Audio(shoot_by_player);
+        shootByPlayerAudio.volume = shootVolume;
+        shootByPlayerAudio.play();
+    }
 
     return (
         <div className='world-container'>
@@ -167,15 +272,17 @@ const World = () => {
                     size={80}
                     baseColor="hsl(219, 84%, 56%)"
                     stickColor="hsl(219, 84%, 30%)"
-                    move={handleMove}
-                    stop={handleStop}
+                    move={(e) => moveHandler(moveKeys[e.direction])}
+                    stop={stopHandler}
                 ></Joystick>
             </ControlPanel>
             <div className='playground-container'>
                 {game_start? 
                     <>
                         <Map />
-                        {(!game_pause && player.position.length > 0) && <Player player={player}/>}
+                        {(!game_pause && player.position.length > 0) && 
+                            <Player player={{...player, rotate:rotate, posRatio:posRatio}} 
+                                fireHandler={fireHandler} attemptMove={attemptMove} moveHandler={moveHandler} stopHandler={stopHandler}/>}
                         {!game_pause && tanks.map(tank =>
                             <Tank key={tank.key_index} tank={{...tank, imageUrl: enemyTank}} />)}
                         {(game_over || game_win) && <GameResult gameResultData={getGameResultData()} />}
